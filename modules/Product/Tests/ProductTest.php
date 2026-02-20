5e666a56c4317901274fdc01ece57494dbf3d62d
mana007777 <e7ifubbb@gmail.com>
2026-02-21 02:49:32 +0300

    PHPUnit Product Test

---
+++ modules/Product/Tests/ProductTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Modules\Product\Tests;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Modules\Order\Models\Order;
+use Modules\Order\Models\OrderLine;
+
+class ProductTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+   public function test_it_works()
+    {
+        $this->assertTrue(true);
+    }
+}
